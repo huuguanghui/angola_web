2421d40027485c20658728024649a90387f1aa04
starkingwx <starkingwx@gmail.com>
2012-11-15 19:33:37 +0800

    return userkey when pwd is changed successfully

--- src/com/angolacall/mvc/controller/ProfileApiController.java
+++ src/com/angolacall/mvc/controller/ProfileApiController.java
@@ -8,11 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.angolacall.framework.ContextLoader;
+import com.angolacall.web.user.UserBean;
 import com.richitec.ucenter.model.UserDAO;
 import com.richitec.util.MD5Util;
 
@@ -33,7 +36,7 @@ public class ProfileApiController {
 			@RequestParam(value = "username") String userName,
 			@RequestParam(value = "oldPwd") String oldPwd,
 			@RequestParam(value = "newPwd") String newPwd,
-			@RequestParam(value = "newPwdConfirm") String newPwdConfirm) throws IOException {
+			@RequestParam(value = "newPwdConfirm") String newPwdConfirm) throws IOException, JSONException {
 		log.info("countryCode: " + countryCode + " username: " + userName + " oldPwd: " + oldPwd + " newpwd: " + newPwd + " confirmpwd: " + newPwdConfirm);
 		Map<String, Object> user = userDao.getUser(countryCode, userName);
 		String pwd = (String) user.get("password");
@@ -54,5 +57,10 @@ public class ProfileApiController {
 			return;
 		}
 		
+		Map<String, Object> userMap = userDao.getUser(countryCode, userName);
+		String userkey = (String) userMap.get("userkey");
+		JSONObject ret = new JSONObject();
+		ret.put("userkey", userkey);
+		response.getWriter().print(ret.toString());
 	}
 }
