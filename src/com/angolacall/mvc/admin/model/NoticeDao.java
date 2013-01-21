7d540f078db94802964983392f69f5138873c514
starkingwx <starkingwx@gmail.com>
2013-01-21 17:21:03 +0800

    now can send notice to user
    send notice when system give gift money to user

--- src/com/angolacall/mvc/admin/model/NoticeDao.java
+++ src/com/angolacall/mvc/admin/model/NoticeDao.java
@@ -20,9 +20,24 @@ public class NoticeDao {
 		jdbc = new JdbcTemplate(ds);
 	}
 	
+	/**
+	 * add notice to all user
+	 * @param content
+	 */
 	public void addNotice(String content) {
 		String sql = "INSERT INTO notices (content) VALUES(?)";
 		jdbc.update(sql, content);
+	}
+	
+	/**
+	 * send notice to specified user
+	 * @param countryCode
+	 * @param userName
+	 * @param content
+	 */
+	public void sendNoticeToUser(String countryCode, String userName, String content) {
+		String sql = "INSERT INTO notices (content, status, to_user) VALUES(?,?,?)";
+		jdbc.update(sql, content, NoticeStatus.publish.name(), countryCode + userName);
 	}
 	
 	public void hideNotice(Integer id) {
@@ -44,17 +59,20 @@ public class NoticeDao {
 	}
 	
 	public List<Map<String, Object>> getNotices() {
-		String sql = "SELECT id, content, UNIX_TIMESTAMP(createtime) AS time, status FROM notices WHERE status <> ? ORDER BY createtime DESC";
+		String sql = "SELECT id, content, UNIX_TIMESTAMP(createtime) AS time, status FROM notices WHERE to_user = 'all' AND status <> ? ORDER BY createtime DESC";
 		return jdbc.queryForList(sql, NoticeStatus.hidden.name());
 	}
 	
 	/**
 	 * get the published notices whose id is larger than currentId
 	 * @param currentId
+	 * @param countryCode
+	 * @param userName
+	 * 
 	 * @return
 	 */
-	public List<Map<String, Object>> getNewPublishedNotices(Integer currentId) {
-		String sql = "SELECT id, content, UNIX_TIMESTAMP(createtime) AS time, status FROM notices WHERE status = ? AND id > ?";
-		return jdbc.queryForList(sql, NoticeStatus.publish.name(), currentId);
+	public List<Map<String, Object>> getNewPublishedNotices(Integer currentId, String countryCode, String userName) {
+		String sql = "SELECT id, content, UNIX_TIMESTAMP(createtime) AS time, status FROM notices WHERE DATE(createtime) >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH) AND status = ? AND id > ? AND (to_user = ? OR to_user = ?) ORDER BY createtime DESC";
+		return jdbc.queryForList(sql, NoticeStatus.publish.name(), currentId, countryCode + userName, "all");
 	}
 }
