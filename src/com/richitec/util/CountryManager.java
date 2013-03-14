6a9cb8c95e75244710e617797bd543d20bbbe47d
starkingwx <starkingwx@gmail.com>
2013-03-14 18:27:59 +0800

    add HongKong and Thailand country code

--- src/com/richitec/util/CountryManager.java
+++ src/com/richitec/util/CountryManager.java
@@ -9,7 +9,8 @@ public class CountryManager {
 
 	private Map<String, String> countryMap;
 	private static String[] COUNTRY_CODES = { "0086", "086", "+86", "86",
-			"00244", "0244", "+244", "244", "0057", "057", "+57", "57" };
+			"00244", "0244", "+244", "244", "0057", "057", "+57", "57", "00852", "0852", "+852", "852",
+			"0066", "066", "+66", "66"};
 
 	private CountryManager() {
 		countryMap = new HashMap<String, String>();
@@ -20,6 +21,8 @@ public class CountryManager {
 		countryMap.put("0057", "co");
 		countryMap.put("0086", "cn");
 		countryMap.put("00244", "ao");
+		countryMap.put("00852", "hk");
+		countryMap.put("0066", "th");
 	}
 
 	public synchronized static CountryManager getInstance() {
